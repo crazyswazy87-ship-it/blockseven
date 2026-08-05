13d9715cc615c9a6fec59138fc53c6837e2d4b1b
swazy <crazyswazy87@gmail.com>
2026-08-05 05:48:42 -0700

    iriee

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -3,15 +3,15 @@ import { Client, Databases, ID } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint('https://sgp.cloud.appwrite.io/v1')
+  .setProject('695fa78500123148c6ff');
 
 export const databases = new Databases(client);
 
-export const DATABASE_ID =
-  import.meta.env.VITE_APPWRITE_DATABASE_ID;
+export const DATABASE_ID = '696c7dc0000d7998f391'
 
-export const INQUIRIES_COLLECTION_ID =
-  import.meta.env.VITE_APPWRITE_INQUIRIES_COLLECTION_ID;
+
+export const INQUIRIES_COLLECTION_ID = 'inquiries'
+  
 
 export { ID };
